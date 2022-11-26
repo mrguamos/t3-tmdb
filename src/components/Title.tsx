9799c6ac26d5e99600f03ebc7d9c1dfce90bdba0
Mark Ryan Guamos <ryan.guamos@gmail.com>
2022-11-26 19:22:26 +0800

    lato

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 const Title = ({ children }: Props) => {
   return (
-    <span className="text-center text-xl font-normal text-teal-300 md:text-left">
+    <span className="text-center text-2xl font-normal text-teal-300 md:text-left">
       {children}
     </span>
   )
