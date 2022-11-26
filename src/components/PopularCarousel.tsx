e5d7132d76e09aad76e3b7e8094fd16a2e92b61e
Mark Ryan Guamos <ryan.guamos@gmail.com>
2022-11-26 17:16:46 +0800

    lazy

--- src/components/PopularCarousel.tsx
+++ src/components/PopularCarousel.tsx
@@ -2,7 +2,7 @@ import type { PopularMoviesType } from '../types/movies'
 import type { PopularTVSType } from '../types/tv'
 import MediaCard from './MediaCard'
 import { Swiper, SwiperSlide } from 'swiper/react'
-import { FreeMode, Pagination, Navigation } from 'swiper'
+import { Lazy, FreeMode, Pagination, Navigation } from 'swiper'
 import 'swiper/css'
 import 'swiper/css/pagination'
 import 'swiper/css/navigation'
@@ -15,13 +15,14 @@ const PopularCarousel = ({ items }: Props) => {
   return (
     <Swiper
       freeMode
+      lazy={true}
       slidesPerView={'auto'}
       slidesPerGroup={1}
       loop={true}
       centeredSlides
       loopFillGroupWithBlank={true}
       navigation={true}
-      modules={[FreeMode, Pagination, Navigation]}
+      modules={[Lazy, FreeMode, Pagination, Navigation]}
       spaceBetween={30}
       className={'flex w-full'}
     >
