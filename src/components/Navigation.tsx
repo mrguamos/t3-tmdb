8bd69a8dadb147e81330262741d00b65de2c5913
Mark Ryan Guamos <ryan.guamos@gmail.com>
2022-12-08 23:54:11 +0800

    code cleanup

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -7,14 +7,14 @@ import NavigationIcon from './NavigationIcon'
 const Navigation = () => {
   return (
     <div className="mx-10 mb-10 flex justify-evenly border-b-2 border-b-zinc-500 pb-3 ">
-      <div className="flex  justify-center">
+      <div className="flex justify-center">
         <NavigationIcon href="/" Icon={GoHome} />
       </div>
 
       <div className="flex justify-center">
         <NavigationIcon href="/tvs" Icon={MdLiveTv} />
       </div>
-      <div className="order-zinc-500 flex  justify-center">
+      <div className="flex justify-center">
         <NavigationIcon href="/movies" Icon={BiCameraMovie} />
       </div>
     </div>
